e6b7e7766a3481de7d4441d178f07fd6192674ee
Andros Fenollosa <andros@fenollosa.email>
2019-09-17 16:33:13 +0200

    Update config email

--- src/tadam/email.clj
+++ src/tadam/email.clj
@@ -7,17 +7,18 @@
 (defn send
   "Add session in request"
   [to subject content-html content-plain]
-  (send-message {:host (env :email-host)
-                 :user (env :email-user)
-                 :pass (env :email-pass)
-                 :ssl  (env :email-ssl)
-                 :port (env :email-port)}
-                {:from    (env :email-from)
-                 :to      to
-                 :subject subject
-                 :type    "text/html; charset=utf-8"
-                 :body    [:alternative
-                           {:type    "text/plain;charset=utf-8"
-                            :content (env content-plain)}
-                           {:type    "text/html;charset=utf-8"
-                            :content content-html}]}))
+  (let [env-email (clojure.edn/read-string (:email env))]
+    (send-message {:host (:host env-email)
+                   :user (:user env-email)
+                   :pass (:pass env-email)
+                   :ssl  (:ssl env-email)
+                   :port (:port env-email)}
+                  {:from    (:from env-email)
+                   :to      to
+                   :subject subject
+                   :type    "text/html; charset=utf-8"
+                   :body    [:alternative
+                             {:type    "text/plain;charset=utf-8"
+                              :content content-plain}
+                             {:type    "text/html;charset=utf-8"
+                              :content content-html}]})))
