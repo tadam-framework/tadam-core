08694d642d580b46f29ec219cc7ba8c53eab8543
Andros Fenollosa <andros@fenollosa.email>
2020-01-19 09:41:37 +0100

    Add new test

--- test/tadam/templates_test.clj
+++ test/tadam/templates_test.clj
@@ -4,4 +4,6 @@
 
 (deftest raw-HTML
   (testing "Raw HTML simple"
-    (is (= (templates/raw-HTML "<html><body></body></html>" {}) {:status 200, :headers {"Content-Type" "text/html"}, :body "<html><body></body></html>"}))))
+    (is (= (templates/raw-HTML "<html><body></body></html>" {}) {:status 200, :headers {"Content-Type" "text/html"}, :body "<html><body></body></html>"})))
+  (testing "Raw HTML with param"
+    (is (= (templates/raw-HTML "<html><body>{{ hello }}</body></html>" {:hello "world"}) {:status 200, :headers {"Content-Type" "text/html"}, :body "<html><body>world</body></html>"}))))
