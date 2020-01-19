0cbbeaaf5b56c2da7baa839583808cba41f2bce2
Andros Fenollosa <andros@fenollosa.email>
2020-01-19 13:16:46 +0100

    Update version 0.3.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject tadam "0.2.11"
+(defproject tadam "0.3.0"
   :description "First version"
   :url "https://github.com/tadam-framework/tadam"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
